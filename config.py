92798271af582e0bf7532f5c56c411f0c22f12e6
Cyanidetitan <122152717+Cyanidetitan@users.noreply.github.com>
2023-11-01 18:45:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "5222572158:AAENHtTOnhWBh4UUZKTjq5ruMtil_4zRA_0")
-API_ID = int(os.environ.get("API_ID", "8858279"))
+API_ID = 8858279
 API_HASH = os.environ.get("API_HASH", "ef28c3f458143cbcb4271a98a2e9d596")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001841958077"))
 AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "5894098166").split())
